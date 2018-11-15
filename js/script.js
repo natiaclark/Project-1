3fceca3426afb48f0a28b4e7d9cb6e365414cbc5
natiaclark <tia927@hotmail.com>
2018-11-15 15:59:28 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ FSJS project 1 - A Random Quote Generator
 ******************************************/
 
 // set up my array function with quotes from an online website.
-//
-var quotes= [
+
+  var quotes= [
 {
   quote:'If you don\’t like something, change it.  If you can’t change it, change your attitude.',
   source: 'Maya Angelou',
