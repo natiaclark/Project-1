336b06ca2af714d25a1f3480ade99e1dbba88f45
natiaclark <tia927@hotmail.com>
2018-11-17 12:46:12 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var quotes= [
 {
   quote: "If you don/t like something change it.  If you can/t change it change your attitude.",
   source:"Maya Angelou",
-  
+  citation: "keepinspiring.me, 2011",
 },
 {
   quote:"People die but legends live forever.",
@@ -30,28 +30,33 @@ var quotes= [
   source:'Frank Sinatra',
 
 },
+  {
+   quote:"You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.",
+   source:"Dr.Suess",
+   citation: "Keepinspiring.me, 2011"
+  
+  },
 
 ];
 
 
- //Create the `getRandomQuote` 
+ //This function selects and return random quotes from the array
   function getRandomQuote(array) {
     return array[Math.floor(Math.random() * array.length)];
  }
 
-// function printQuote:
+// This function display quote array, and the first variable stores getRandomQuote function
  function printQuote() {
   //Call `getRandomQuote` and assign to `getQuote`
   var getQuote = getRandomQuote(quotes);
-    //Initialize variable to store our quote/property html expression
+    //This variable stores the quote and source string
     var html = '';
-    //Every quote will have quote + source properties.. add to html string.
     html += '<p class="quote">' + getQuote.quote + '</p>' + '<p class="source">' + getQuote.source;
     //Check if quote has citation property, if so add to html string.
     if (getQuote.hasOwnProperty('citation')) {
       html += '<span class="citation">' + getQuote.citation + '</span>';
     }
-   //Check if quote has year property.
+   //Check if quote has year property, add it to html string
     if (getQuote.hasOwnProperty('year')) {
       html += '<span class="year">' + getQuote.year + '</span>';
     }
