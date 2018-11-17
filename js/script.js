a52c90930a820b66881f6546dab36110a5ebba62
natiaclark <tia927@hotmail.com>
2018-11-17 09:08:52 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -93,42 +93,18 @@ function printQuote() {
   
   }
   
-  function changeBackgroundColor() {
-    //Call `getRandomRBG` 3x and assign to `backgroundRGB`: rbg(x,x,x) values.
-    var backgroundRGB = 'rgb(' + getRandomRGB() + ',' + getRandomRGB() + ',' + getRandomRGB() + ')';
-    //Reference the body background color and store new value.
-    var outputColor = document.body.style.background = backgroundRGB;
-    //Reference button background color and set it = to new background color.
-    var buttonColor = document.getElementById('loadQuote').style.background = outputColor;
-    //Return the new color!
-    return outputColor, buttonColor;
-
-  
-  
-  
-  }
-
-
-
-      
-     document.getElementById('quote-box').innerHTML = quoteProps;
+  document.getElementById('quote-box').innerHTML = quoteProps;
       
 
-    }
+   
 
 //"show another Quote" click button
 
-document.getElementById('loadQuote').addEventListener("click", function() {
-    printQuote();
-  changeBackgroundColor();
-});
+document.getElementById('loadQuote').addEventListener("click", printQuote) 
+    
+}
 
-//Init interval timer
-setInterval(function() {
-  //Load new quote + background color!
-  printQuote();
-  changeBackgroundColor();
-}, 30000);
+
 
 
 
