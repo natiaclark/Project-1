385de487a12ffad1d897dbd1ad14e53ab3167b9c
natiaclark <tia927@hotmail.com>
2018-11-17 09:24:05 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,46 +40,33 @@ var quotes= [
  }
 
 // function printQuote:
-
-function printQuote() {
-
+ function printQuote() {
   //Call `getRandomQuote` and assign to `getQuote`
-
   var getQuote = getRandomQuote(quotes);
-
     //Initialize variable to store our quote/property html expression
-
     var html = '';
-
     //Every quote will have quote + source properties.. add to html string.
-
     html += '<p class="quote">' + getQuote.quote + '</p>' + '<p class="source">' + getQuote.source;
-
     //Check if quote has citation property, if so add to html string.
-
     if (getQuote.hasOwnProperty('citation')) {
-
       html += '<span class="citation">' + getQuote.citation + '</span>';
-
     }
-
     //Check if quote has year property, if so add to html string
-
     if (getQuote.hasOwnProperty('year')) {
-
       html += '<span class="year">' + getQuote.year + '</span>';
-
     }
-
     //Check if quote has category property, if so add to html string
-
     if (getQuote.hasOwnProperty('category')) {
-
       html += getQuote.category;
+    }
+    //Close html 2nd <p> of html string to keep all secondary props on same line.
+    html += '</p>';
+    //Reference the html container for the qutoe and output the quote and corresponding properties!
+    var outputQuote = document.getElementById('quote-box');
+    outputQuote.innerHTML = html;
+    return outputQuote;
+}
 
-   
-  
-  }
   
   document.getElementById('quote-box').innerHTML = quoteProps;
       
